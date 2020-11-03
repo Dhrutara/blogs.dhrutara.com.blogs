44f6455a591856522f9e18e60d859dce230b0c02
Ramesh Kanjinghat <RameshK@kaplantrucking.com>
2020-11-03 17:16:05 -0500

    Added code renderer component

--- react-markdown/src/App.js
+++ react-markdown/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import ImageRenderer from '../src/components/image-renderer';
 import HeaderRenderer from '../src/components/header-renderer';
+import CodeRenderer from '../src/components/code-block-renderer';
 import ReactMarkdown from 'react-markdown';
 
 const MarkDown = `> Sample markdown that will be rendered as html 
+
+**Sample Headers**
 # Header1
 ## Header2
 ### Header3
@@ -12,9 +15,21 @@ const MarkDown = `> Sample markdown that will be rendered as html
 ###### Header6
 
 
-> This image will be rendered as html element figure with figurecaption.
+**This image will be rendered as html element figure with figurecaption.**
 ![A sample image](/images/Screenshot.jpg) 
 
+**Below code will be rendered with line numbers and line highlights**
+
+\`\`\`javascript{1,3,5}
+const sampleFunction = () => {
+
+  const value = '1'
+  let language = value;
+  let lineHighlights = [];
+  const obj = { 'language': language, 'lineHighlights': lineHighlights }; 
+  return obj
+}
+\`\`\`
 
 > Complete`;
 
@@ -22,7 +37,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <ReactMarkdown source={MarkDown} escapeHtml={false} renderers={{ "image": ImageRenderer, "heading": HeaderRenderer}} />
+        <ReactMarkdown source={MarkDown} escapeHtml={false} renderers={{ "image": ImageRenderer, "heading": HeaderRenderer, "code": CodeRenderer }} />
       </div>
     </div>
   );
